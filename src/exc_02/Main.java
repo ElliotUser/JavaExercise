9f5dbe5726fe1efbe851df91e649f051ac544521
ElliotUser <andersen.e@bk.ru>
2019-02-04 18:50:13 +0300

    Уложить доминошки.

---
+++ src/exc_02/Main.java
@@ -0,0 +1,23 @@
+package exc_02;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    void solve(Scanner in, PrintWriter out){
+        int m = in.nextInt();
+        int n = in.nextInt();
+        out.println(m * n/2);
+    }
+
+    void run(){
+        try (Scanner in = new Scanner(System.in);
+             PrintWriter out = new PrintWriter(System.out)){
+
+            solve(in,out);
+        }
+    }
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
